a822d4d8797a5ef5a3fcd862d38b3e86858b5a81
Εμμανουήλ Περπιράκης <139139695+ManosPerpirakes@users.noreply.github.com>
2023-10-09 14:32:48 +0300

    major update and bug fixes

--- game_files/main.py
+++ game_files/main.py
@@ -1,3 +1,4 @@
+from random import randint
 from pygame import *
 
 font.init()
@@ -17,12 +18,12 @@ class Plane():
         global planeinrunway2
         altitude = 750 - player.rect.y
         keys = key.get_pressed()
-        if (keys[K_d] or keys[K_RIGHT]) and speedx < 1002:
+        if (keys[K_d] or keys[K_RIGHT] or autopilot) and speedx < 1002:
             speedx += 2
-        if (keys[K_a] or keys[K_LEFT]) and speedx > 10:
+        if (keys[K_a] or keys[K_LEFT]) and speedx > 10 and (not autopilot):
             for i in runways:
                 if self.rect.colliderect(i):
-                    speedx -= 10
+                    speedx -= 6
             else:
                 speedx -= 3
             if speedx < 10:
@@ -32,6 +33,8 @@ class Plane():
                 speedy += 0.1
         if keys[K_s] or keys[K_DOWN]:
             speedy -= 0.1
+        if autopilot:
+            speedy = 0
         if speedx > 0:
             speedx -= 0.1
         counterlocal = 1
@@ -68,9 +71,18 @@ while closeall != True:
     runway = rect.Rect(0, 700, 100000, 50)
     runway2 = rect.Rect(10000000, 700, 100000, 50)
     runways = [runway, runway2]
+    buildings = []
+    colours = []
+    for i in range(10000):
+        height = randint(50, 200)
+        buildings.append(rect.Rect((randint(120000, (10000000 - 20000))), (750 - height), randint(50, 100), height))
+        colour = randint(50, 150)
+        colours.append((colour, colour, colour))
+    pause = False
     distancetorunway = round(runway2.x - (player.rect.x + 100), 1)
     speedx = 0
     speedy = 0
+    autopilot = False
     planeinrunway1 = False
     timevar = 0
     planeinrunway2 = False
@@ -81,25 +93,53 @@ while closeall != True:
             if i.type == QUIT:
                 close = True
                 closeall = True
+            if i.type == KEYDOWN:
+                if i.key == K_1:
+                    if pause:
+                        pause = False
+                    else:
+                        pause = True
+                if i.key == K_e:
+                    if autopilot:
+                        autopilot = False
+                    else:
+                        autopilot = True
         if closeall:
             close = True
         for i in runways:
             draw.rect(w, (100, 100, 100), i)
+            if player.rect.colliderect(i):
+                autopilot = False
         player.showairplane()
-        player.move()
+        if not pause:
+            player.move()
+        else:
+            w.blit(font.SysFont('Arial', 60).render("Game paused", True, (0, 0, 0)), (100, 100))
         w.blit(font.SysFont('Arial', 30).render('speed: ' + str(speedx) + "km/h", True, (0, 0, 0)), (1000, 100))
         w.blit(font.SysFont('Arial', 30).render('climb: ' + str(speedy) + "deg", True, (0, 0, 0)), (1000, 140))
         w.blit(font.SysFont('Arial', 30).render('altitude: ' + str(altitude) + "m", True, (0, 0, 0)), (1000, 180))
         w.blit(font.SysFont('Arial', 30).render('distance to runway: ' + str(distancetorunway) + "m", True, (0, 0, 0)), (1000, 220))
+        if autopilot:
+            w.blit(font.SysFont('Arial', 30).render("Autopilot: ON", True, (0, 0, 0)), (1000, 260))
+        else:
+            w.blit(font.SysFont('Arial', 30).render("Autopilot: OFF", True, (0, 0, 0)), (1000, 260))
         distancetorunway = round(runway2.x - (player.rect.x + 100), 1)
         if player.rect.colliderect(runway2) and speedx == 0:
             close = True
             win = True
         timevar += 1
+        counter = 0
+        for i in buildings:
+            draw.rect(w, colours[counter], i)
+            if not pause:
+                i.x -= speedx
+            if player.rect.colliderect(i):
+                close = True
+            counter += 1
         display.update()
         clock.tick(60)
     close = False
-    timevar /= 360
+    timevar /= 3600
     while close != True:
         w.fill((100, 100, 255))
         for i in event.get():
